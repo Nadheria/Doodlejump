1abd03cde7dbdbffc69a4593343320f5f4a564f5
Anthony <anthony7richelle@gmail.com>
2023-05-02 13:30:52 +0200

    Jetpack

--- app/src/main/java/com/doodlejump/monsters/Monster.kt
+++ app/src/main/java/com/doodlejump/monsters/Monster.kt
@@ -9,10 +9,14 @@ import com.doodlejump.Player
 class Monster(pos0: Vector): GameObject(Vector(311F, 272F), pos0, R.drawable.monster){
 
     override fun whenHit(player: Player) {
-        if(player.speed.y < 0 ) removed = true
-        player.rebound()
-        if(player.speed.y > 0) player.speed.y = -100F
-        player.die()
+        if(player.speed.y < 0 ) {
+            removed = true
+            player.rebound()
+        }
+        if(player.speed.y > 0) {
+            player.die()
+            player.speed.y = - 50F
+        }
     }
 
 
