2aae15ae6ef7b340538a48ce89565f19085363a0
Athemys <anthony7richelle@gmail.com>
2023-05-04 18:39:16 +0200

    Added an interface and made the collisions box better

--- app/src/main/java/com/doodlejump/plateforms/BasePlatform.kt
+++ app/src/main/java/com/doodlejump/plateforms/BasePlatform.kt
@@ -3,4 +3,4 @@ package com.doodlejump.plateforms
 import com.doodlejump.R
 import com.doodlejump.Vector
 
-class BasePlatform(var ipos: Vector): Platform(ipos, R.drawable.baseplateform) { }
+class BasePlatform(ipos: Vector): Platform(ipos, R.drawable.baseplateform)
